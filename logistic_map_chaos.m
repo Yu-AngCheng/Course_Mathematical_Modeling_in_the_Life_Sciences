f75bf7d2dbd9c78ac8b1ab553b5b42823ae8c104
YuangCheng <yuang_cheng@pku.edu.cn>
2021-04-21 02:53:07 +0800

    Update logistic_map_chaos.m

--- logistic_map_chaos.m
+++ logistic_map_chaos.m
@@ -1,38 +1,17 @@
+%% show the chaos 
 clear
 
 r = 4;
 f = @(x) r.*x.*(1-x);
-tmax = 1000000;
-figure
-x_temp = 0 :0.001:1;
-h1 = plot(x_temp,f(x_temp),'k','LineWidth',1); % the function f
-hold on
-h2 = plot(x_temp,x_temp,'k','LineWidth',1); % the diagonal line
-
+tmax = 10000;
 xinit = 0.6;
 x1 = NaN(1,tmax);x1(1) = xinit;
-hold on
-plot([xinit,xinit],[0,f(xinit)],'Color','b','LineWidth',0.5);
-for t = 2:tmax
-    x1(t) = f(x1(t-1));
-    hold on;
-    plot([x1(t-1),x1(t)],[x1(t),x1(t)],'b','LineWidth',0.5);
-    hold on
-    plot([x1(t),x1(t)],[x1(t),f(x1(t))],'b','LineWidth',0.5);
-end
-
 xinit = 0.6000001;
 x2 = NaN(1,tmax);x2(1) = xinit;
-hold on
-plot([xinit,xinit],[0,f(xinit)],'Color','r','LineWidth',0.5);
 for t = 2:tmax
+    x1(t) = f(x1(t-1));
     x2(t) = f(x2(t-1));
-    hold on;
-    plot([x2(t-1),x2(t)],[x2(t),x2(t)],'r','LineWidth',0.5);
-    hold on
-    plot([x2(t),x2(t)],[x2(t),f(x2(t))],'r','LineWidth',0.5);
 end
-axis square
 
 % show the process
 % x1 and x2 only share the same points only at early iterations and differ
@@ -43,7 +22,9 @@ hold on
 plot(x2);
 xlabel('Iterations');
 ylabel('x_n');
+xlim([0,50])
 
+% show the distribution
 % But x1 and x2 have the same distribution
 % For better illustration, choose tmax =10000;
 figure
