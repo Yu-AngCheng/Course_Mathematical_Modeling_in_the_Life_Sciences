7ad99c9de1be85ed6cbd0346b234b076500a29c0
YuangCheng <yuang_cheng@pku.edu.cn>
2021-06-12 18:52:55 +0800

    Create preprocess_single_neuron.py

---
+++ Project/preprocess_single_neuron.py
@@ -0,0 +1,58 @@
+import os
+import numpy as np
+
+# Data retrieval
+fname = []
+for j in range(3):
+    fname.append('steinmetz_part%d.npz' % j)
+    if not os.path.isfile(fname[j]):
+        raise Exception("!!! No data !!!")
+
+# Data loading
+alldat = np.array([])
+for j in range(len(fname)):
+    alldat = np.hstack((alldat, np.load('steinmetz_part%d.npz' % j, allow_pickle=True)['dat']))
+
+# sort the spikes by different brain groups
+brain_groups = ["VISa", "VISam", "VISl", "VISp", "VISpm", "VISrl",  # visual cortex
+                "CL", "LD", "LGd", "LH", "LP", "MD", "MG", "PO", "POL", "PT", "RT", "SPF", "TH", "VAL", "VPL", "VPM",
+                # thalamus
+                "CA", "CA1", "CA2", "CA3", "DG", "SUB", "POST",  # hippocampal
+                "ACA", "AUD", "COA", "DP", "ILA", "MOp", "MOs", "OLF", "ORB", "ORBm", "PIR", "PL", "SSp", "SSs", "RSP",
+                " TT",  # non-visual cortex
+                "APN", "IC", "MB", "MRN", "NB", "PAG", "RN", "SCs", "SCm", "SCig", "SCsg", "ZI",  # midbrain
+                "ACB", "CP", "GPe", "LS", "LSc", "LSr", "MS", "OT", "SNr", "SI",  # basal ganglia
+                "BLA", "BMA", "EP", "EPd", "MEA"  # cortical subplate
+                ]
+nGroup = len(brain_groups)
+
+for k in range(len(alldat)):
+
+    dat = alldat[k]
+    tmp = dat["spks"].shape  # Neuron * Trial * Time Bin
+    nNeuron = tmp[0]
+    nTrial = tmp[1]
+    nBin = tmp[2]
+
+    nareas = nGroup  # only the top nareas regions are taken into account
+    NN = len(dat['brain_area'])  # number of neurons
+    groupindex = nGroup * np.ones(NN, )  # last one is "other"
+    for j in range(nareas):
+        groupindex[np.isin(dat['brain_area'], brain_groups[j])] = j
+    dat['groupindex'] = groupindex
+
+    groupSpk = np.full((0, nTrial, nBin), np.nan)
+    groupindex_sorted = np.array([])
+    for i in range(nareas):
+        number_neurons = np.sum(groupindex == i)
+        if number_neurons != 0:
+            groupSpk = np.concatenate((groupSpk, dat['spks'][groupindex == i, :, :]), axis=0)
+            groupindex_sorted = np.concatenate((groupindex_sorted, np.tile(i, number_neurons)))
+    dat['groupSpk'] = groupSpk
+    dat['groupindex_sorted'] = groupindex_sorted
+
+    dat['session'] = np.tile(k, nTrial)
+
+    alldat[k] = dat
+
+np.save('Steinmetz_main_singleneuron.npy', alldat)
